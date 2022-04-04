6992d07937ad9b776ddfd4e1436ca815079a5810
Lindse1 <93844220+Lindse1@users.noreply.github.com>
2022-04-04 18:15:21 -0400

    Start Screen Scene

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject MainMenuUI;
+
+    [SerializeField]
+    private string startLevelName;
+
+    private void Awake()
+    {
+        //if (PlayerPrefs.GetFloat("gameVolume") >= 1)
+        //{
+        //    PlayerPrefs.SetFloat("gameVolume", 0.25f);
+        //    Debug.Log("We set volume to" + PlayerPrefs.GetFloat("gameVolume"));
+        //    AudioListener.volume = PlayerPrefs.GetFloat("gameVolume");
+        //}
+        //else
+        //{
+        //    //Debug.Log("We set volume to" + PlayerPrefs.GetFloat("gameVolume"));
+        //}
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("MainLevel");
+    }
+}
